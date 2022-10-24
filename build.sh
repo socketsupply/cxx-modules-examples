0f1df58dafbf27b67c5731d827e086f4bb4a723c
heapwolf <paolo@socketsupply.co>
2022-10-24 11:12:17 +0200

    tidy

--- build.sh
+++ build.sh
@@ -8,13 +8,24 @@ if [ ! -d "build" ]; then
   mkdir build
 
   # precompile the `beep` module
-  $CXX -std=c++2b -x c++-module --precompile src/beep.cc -fmodule-name=Socket.Beep -o build/beep.pcm
+  $CXX -std=c++2b              \
+    -x c++-module              \
+    --precompile src/beep.cc   \
+    -fmodule-name=Socket.Beep  \
+    -o build/beep.pcm
+
   # create an object file from the precompiled module that can be linked
   $CXX -std=c++2b -c build/beep.pcm -o build/beep.o
 
   # same procedure for the `quxx` module. order of compilation is important, we
   # need quxx to be precompiled before `boop` because `boop` will use it.
-  $CXX -std=c++2b -x c++-module --precompile src/quxx.cc -fmodule-name=Socket.Quxx -o build/quxx.pcm
+  $CXX -std=c++2b              \
+    -x c++-module              \
+    --precompile src/quxx.cc   \
+    -fmodule-name=Socket.Quxx  \
+    -o build/quxx.pcm
+
+  # create an object file
   $CXX -std=c++2b -c build/quxx.pcm -o build/quxx.o
 
   #
@@ -29,22 +40,24 @@ if [ ! -d "build" ]; then
   # the logical structure of a module.
 
   # `-x c++-module` allows you to specify any extension for a module file
-  $CXX -std=c++2b \
-    -x c++-module \
-    --precompile src/boop.cc \
-    -fimplicit-modules \
-    -fmodule-file=build/quxx.pcm \
-    -fmodule-name=Socket.Boop \
+  $CXX -std=c++2b                 \
+    -x c++-module                 \
+    --precompile src/boop.cc      \
+    -fimplicit-modules            \
+    -fmodule-file=build/quxx.pcm  \
+    -fmodule-name=Socket.Boop     \
     -o build/boop.pcm
 
+  # create an object file
   $CXX -std=c++2b -c build/boop.pcm -o build/boop.o
 fi
 
-$CXX -std=c++2b \
-  -fmodule-file=build/quxx.pcm build/quxx.o \
-  -fmodule-file=build/beep.pcm build/beep.o \
-  -fmodule-file=build/boop.pcm build/boop.o \
-  -o bin/main \
+$CXX -std=c++2b                              \
+  -fmodule-file=build/quxx.pcm build/quxx.o  \
+  -fmodule-file=build/beep.pcm build/beep.o  \
+  -fmodule-file=build/boop.pcm build/boop.o  \
+  -o bin/main                                \
   src/main.cc
 
 ./bin/main
+
